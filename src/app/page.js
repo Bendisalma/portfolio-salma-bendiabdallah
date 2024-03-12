3c826e09ef3a5532764caeaa552dce4af12bf208
Bissa <bendi.salma@gmail.com>
2024-03-12 01:54:17 -0400

    favicon

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,16 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
     <main className="flex flex-col min-h-screen bg-[#121212] ">
+      
       <Navbar />
       <div className="container mx-auto my-12 px-4 py-4">
         <HeroSection />
@@ -16,6 +22,7 @@ export default function Home() {
         <EmailSection />
       </div>
       <Footer />
-    </main>
+      </main>
+      </>
   );
 }
